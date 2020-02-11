f924b4c04cbe69ed0c6e5d668f7a7e8fcdcce1bb
hantmac <hantmac@outlook.com>
2020-02-11 12:05:40 +0800

    add 26

---
+++ pythonSolutions/26.删除排序数组中的重复项.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode.cn id=26 lang=python
+#
+# [26] 删除排序数组中的重复项
+#
+
+# @lc code=start
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        i = 0
+        for j in range(1,len(nums)):
+            if nums[i] != nums[j]:
+                i+=1
+                nums[i] = nums[j]
+        return i+1
+               
+                       
+                
+        
+# @lc code=end
+
